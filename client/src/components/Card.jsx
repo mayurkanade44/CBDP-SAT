41d5bf8613aa929174205b5ca3e66f2f6f62974e
mayurkanade44 <mayurkanade44@gmail.com>
2022-12-10 14:48:49 +0530

    preview of pdf and dco file added

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,19 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
+import FileViewer from "react-file-viewer";
 import { attachFile } from "../redux/documentSlice";
 
 const Card = ({ typeOfFile, name, typeOfService, file }) => {
+  const [view, setView] = useState(false);
+  const [type, setType] = useState("");
   const dispatch = useDispatch();
+
+  const handleView = (file) => {
+    const fileType = file.split(".").pop();
+    setType(fileType);
+    setView(!view);
+  };
 
   return (
     <motion.div layout className="card">
@@ -13,9 +23,22 @@ const Card = ({ typeOfFile, name, typeOfService, file }) => {
           {name}
         </h5>
         <p className="card-text">
-          <b>Services - </b> {typeOfService.join(",")}
+          <b>Services - </b> {typeOfService.join(", ")}
         </p>
-        <button className="btn btn-primary m-1">Preview</button>
+        <button
+          className="btn btn-primary m-1"
+          onClick={() => handleView(file)}
+        >
+          Preview
+        </button>
+        {view && (
+          <div className="modal">
+            <div className="modal-content">
+            <button className="btn btn-danger" onClick={() => setView(!view)}>Close</button>
+              <FileViewer fileType={type} filePath={file} />
+            </div>
+          </div>
+        )}
         <button className="btn btn-success m-1">Download</button>
         <button
           className="btn btn-dark m-1"
