f8c630aa983e5e30aaeef301453945e99e754a6a
mayurkanade44 <mayurkanade44@gmail.com>
2022-12-26 13:43:45 +0530

    changes in landing page carousal

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import new1 from "../images/new.png";
 import bag from "../images/bag.png";
+import first from "../images/fog.jpeg";
+import second from "../images/fog1.jpeg";
+import third from "../images/stq.jpg";
+
 import { latestDocs } from "../redux/documentSlice";
 import { useNavigate } from "react-router-dom";
 
@@ -91,7 +95,7 @@ const Landing = () => {
         })}
       </div>
       <hr />
-      <div className="app">
+      {/* <div className="app">
         <div className="wrapper">
           <div className="container-wrapper">
             <div className="container-img">
@@ -129,6 +133,88 @@ const Landing = () => {
             </div>
           </div>
         </div>
+      </div> */}
+
+      <div
+        id="carouselExampleControls"
+        className="carousel carousel-dark slide"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active" style={{ height: 400 }}>
+            <img src={first} className="img-fluid" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item" style={{ height: 400 }}>
+            <img src={second} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item" style={{ height: 400 }}>
+            <img src={third} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </div>
   );
